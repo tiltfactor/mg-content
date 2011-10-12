7ae3cc79ea61b73dc0510b257786d0f77be1c117
Vincent Van Uffelen <novazembla@gmail.com>
2011-10-12 15:38:15 +0100

    zoom icon for guess what

--- www/protected/modules/games/views/guessWhat/index.php
+++ www/protected/modules/games/views/guessWhat/index.php
@@ -44,11 +44,11 @@
   ?><span></span></h3>
 
 <script id="template-describe-image" type="text/x-jquery-tmpl">
-  <a href="${url_full_size}" rel="zoom" title="${licence_info}"><img src="${url}" alt="game image" /></a>
+  <img src="${url}" alt="game image" /><a href="${url_full_size}" rel="zoom" title="${licence_info}" class="zoom">zoom</a>
 </script>
 
 <script id="template-guess-image" type="text/x-jquery-tmpl">
- <a href="${url_full_size}" rel="zoom" title="${licence_info}"><img src="${url}" alt="game image" /></a>
+ <div><a href="#${image_id}" class="guessthis" title="click to send this image as guess"><img src="${url}" alt="game image" /></a><a href="${url_full_size}" rel="zoom" title="${licence_info}" class="zoom">zoom</a></div>
 </script>
 
 <script id="template-scores" type="text/x-jquery-tmpl">
@@ -107,47 +107,6 @@
 <script id="template-error-hint-word-to-avoid" type="text/x-jquery-tmpl">
   <h1>Ooops</h1><p>Your hint is one of the words to avoid. Please give another hint.</p>
 </script>
-
-<?php
-/*    
- xxx remove
-<script id="template-turn" type="text/x-jquery-tmpl">
-  <div style="text-align:center" class="clearfix">
-    <a href="${url_full_size}" rel="zoom" title="${licence_info}"><img src="${url}" alt="game image" /></a>
-  </div>
+<script id="template-error-hint-given-twice" type="text/x-jquery-tmpl">
+  <h1>Ooops</h1><p>You have already given this hint.</p>
 </script>
-<script id="template-final-summary" type="text/x-jquery-tmpl">
-  <div id="smallholder0"> 
-    <a href="${url_full_size_1}" rel="zoom" title="${licence_info_1}"><img class="scoreimages" src="${url_1}" alt="game image" /></a>
-  </div> 
-  <div id="smallholder1"> 
-    <a href="${url_full_size_2}" rel="zoom" title="${licence_info_2}"><img class="scoreimages" src="${url_2}" alt="game image" /></a>
-  </div> 
-  <div id="smallholder2"> 
-    <a href="${url_full_size_3}" rel="zoom" title="${licence_info_3}"><img class="scoreimages" src="${url_3}" alt="game image" /></a>
-  </div> 
-  <div id="smallholder3"> 
-    <a href="${url_full_size_4}" rel="zoom" title="${licence_info_4}"><img class="scoreimages" src="${url_4}" alt="game image" /></a>
-  </div> 
-</script>
-<script id="template-final-summary-play-once" type="text/x-jquery-tmpl">
-  <div style="text-align:center" class="clearfix">
-    <a href="${url_full_size}" rel="zoom" title="${licence_info}"><img src="${url}" alt="game image" /></a>
-  </div>
-</script>
-
-<script id="template-final-info" type="text/x-jquery-tmpl">
-  <p class="final">Congratulations <b>${user_name}</b>, you scored <b>${current_score}</b> points in this game.</p>
-</script>
-<script id="template-final-tags-new" type="text/x-jquery-tmpl">
-  <p class="tag-info">New tag(s): <b>'${tags_new}'</b> scoring <b>${tags_new_score}</b> point(s)</p>
-</script>
-<script id="template-final-tags-matched" type="text/x-jquery-tmpl">
-  <p class="tag-info">Matched tag(s): <b>'${tags_matched}'</b> scoring <b>${tags_matched_score}</b> point(s).</p>
-</script>
-<script id="template-final-tags-same_as" type="text/x-jquery-tmpl">
-  <p class="tag-info">Same as ${game_partner_name}: <b>'${tags_same_as}'</b>.</p>
-</script>
-
-*/
-?>
