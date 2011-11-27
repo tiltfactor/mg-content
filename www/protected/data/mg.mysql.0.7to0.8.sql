c14aeff6e97c805428409355125871d552e8d384
Vincent Van Uffelen <novazembla@gmail.com>
2011-11-27 22:24:30 +0000

    cleanup for MS4 release

--- www/protected/data/mg.mysql.0.7to0.8.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `stop_word` 
-  DROP COLUMN `active`, 
-  DROP COLUMN `counter`,
-  ADD COLUMN `source` VARCHAR(12) NOT NULL DEFAULT 'import' AFTER `word` ;
--- www/protected/data/mg.mysql.0.8.1to0.8.2.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `mg`.`image_set` ADD COLUMN `last_access_interval` INT NOT NULL DEFAULT 0  AFTER `licence_id` ;
