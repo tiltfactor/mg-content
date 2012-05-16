4c32c1a78e0a9f9143d1b644fade131157871a3c
Robinson Tryon <qubit@runcibility.com>
2012-05-16 18:34:51 -0400

    Add top-level redirect to www/
    This commit adds a top-level redirect to the www/ directory, as that's
    where all the good stuff happens. It also allows us to hand out
    prettier/shorter URLs for our public installs, which is good because
    people have a shorter memory than a computer.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Redirect to Metadata Games.
+ *
+ */
+
+// Redirect from the base directory to www/, because that's where Yii
+// wants all the action to happen.
+header('Location: www/');
+
+?>
