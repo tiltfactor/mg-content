7f6611d1ef3c16fb723c9684281bd244678330fe
Robinson Tryon <qubit@runcibility.com>
2012-06-02 01:26:19 -0400

    Tag Merge: clarify language
    This language is still complicated and could benefit from some more
    careful thought on proper wording.

--- www/protected/modules/admin/views/tag/merge.php
+++ www/protected/modules/admin/views/tag/merge.php
@@ -10,11 +10,10 @@ $this->breadcrumbs = array(
 
 <h1><?php echo Yii::t('app', 'Merge'); ?> Tag <?php echo $tag_from->tag; ?> with <?php echo $tag_to->tag; ?></h1>
 
-<p><b>The new tag name "<?php echo $tag_to->tag; ?>" is already in use please confirm the merge of "<?php echo $tag_from->tag; ?>" (<?php echo $tag_use_count; ?> tag uses) 
+<p><b>The new tag name "<?php echo $tag_to->tag; ?>" is already in use; please confirm the merge of "<?php echo $tag_from->tag; ?>" (<?php echo $tag_use_count; ?> tag uses) 
   with "<?php echo $tag_to->tag; ?>"?</b></p>
 
-<p>All tag uses of "<?php echo $tag_from->tag; ?>" will replaced by tag uses of "<?php echo $tag_to->tag; ?>" but to each of the tag uses affected
-  a tag orinal version record will be added to allow to account for this tag merge. 
+<p>All tag uses of "<?php echo $tag_from->tag; ?>" will replaced by tag uses of "<?php echo $tag_to->tag; ?>." An original version record will be added to each replaced tag to allow to account for this tag merge. 
 </p>
 
 <p><?php echo CHtml::link(Yii::t('app', 'cancel'), array('update', 'id' => $tag_from->id)); ?> / <?php echo CHtml::link(Yii::t('app', 'merge'), array('merge', 'from_id' => $tag_from->id, 'to_id' => $tag_to->id)); ?></p>
