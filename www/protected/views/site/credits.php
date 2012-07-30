3c337d864c4b966e6073eaa83165493e095a9b7b
Robinson Tryon <qubit@runcibility.com>
2012-07-30 18:49:44 -0400

    credits: Merge credits information from the tilt website

--- www/protected/views/site/credits.php
+++ www/protected/views/site/credits.php
@@ -13,19 +13,25 @@ $this->pageTitle="Credits";
 <p><strong>Project Directors</strong><br />
 Mary Flanagan, Tiltfactor Laboratory Dartmouth College<br />
 Peter Carini, Rauner Library Dartmouth College<br />
-Sukdith Punjasthitkul, Tiltfactor Laboratory Dartmouth College<br />
+</p>
+
+<p><strong>General Support and Project Coordination</strong><br />
+Sukdith Punjasthitkul<br />
+Furtherfield.org<br />
 </p>
 
 <p><strong>Programming Team</strong><br />
 Lead Programmer: Vincent Van Uffelen<br />
 Programmer/Designer: Robinson Tryon<br />
 Additional Programming: Sukdith Punjasthitkul<br />
+Game Programming: Robinson Tryon, Charlie Whitney, Cecile Williams, Joshua Weinberg<br />
+Prototype: Joshua Weinberg<br />
 </p>
 
 <p><strong>Game Design</strong><br />
-Front end and game designer: Zara Downs<br />
+Front end and game designer: Mary Flanagan, Zara Downs<br />
 Additional front end and game design: Sukdith Punjasthitkul<br />
-Student Designer: Max Seidman
+Student Designers: Linden Vongsathorn, Brendan Scully, Cole Ott, Max Seidman
 </p>
 
 <p><strong>QA</strong><br />
@@ -37,8 +43,16 @@ Lead Sysadmin: Robinson Tryon<br />
 Sysadmin/Networking: Mike Pettinicchio, Joe Hill, Sean Dunten<br />
 </p>
 
+<p><strong>Advisory Board</strong><br />
+Brian Brantner – Computer Scientist at Adobe Systems<br />
+Dr. Drew Davidson – Entertainment Technology Center Program Director, Pittsburgh, Carnegie Mellon University<br />
+Tracy Fullerton – Associate Professor and Chair, USC School of Cinematic Arts,
+Interactive Media Division; Director, Electronic Arts Game Innovation Lab; and EA Endowed Chair in Interactive Entertainment<br />
+Dr. Celia Pearce – Director, Experimental Game Lab, School of Literature, Communication &amp; Culture, Georgia Institute of Technology<br />
+</p>
+
 <p><strong>Testers, helpers, and friends:</strong><br />
-Joshua Shaw, Erika Murillo, Viviana Ramos, Robert McAvinue, Shaun Akhtar, Edward del Rosario, Rose Flanagan, Derrick Smith, Nick and Tina Moschella, Courtney Hook, Mehta and Isara Punjasthitkul, Nick Sinnott Armstrong, Matt Geer, Rebekah Scott, Leah Nicolich-Henkin, Jack Litherland, and everyone else who has tagged an image or six. Thank you!
+Joshua Shaw, Erika Murillo, Viviana Ramos, Robert McAvinue, Shaun Akhtar, Edward del Rosario, Rose Flanagan, Derrick Smith, Nick and Tina Moschella, Courtney Hook, Mehta and Isara Punjasthitkul, Nick Sinnott-Armstrong, Matt Geer, Rebekah Scott, Leah Nicolich-Henkin, Jack Litherland, Alicia Driscoll, Max Seidman, and everyone else who has tagged an image or six. Thank you!
 </p>
 
 
