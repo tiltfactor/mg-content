18fbcb9b421bb9752fabab70634aefecb5cca056
Robinson Tryon <qubit@runcibility.com>
2013-05-20 09:13:48 -0400

    Mobile: Clone NexTag as new game 'Mobile'
    The new game 'Mobile' will serve as a stub/example for people writing
    games for mobile devices.
    
    (Indeed, some of our future games might use this example and *not* be
    written for a mobile device, but this will be our primary use-case, at
    least for now)

---
+++ www/protected/modules/games/controllers/MobileController.php
@@ -0,0 +1,127 @@
+<?php
+
+class MobileController extends GxController
+{
+  
+	public function filters() {
+    return array(
+      'IPBlock',
+      'accessControl - index', 
+      );
+  }
+  
+  public function accessRules() {
+    return array(
+        array('allow', 
+          'actions'=>array('index'),
+          'users'=>array('*'),
+          ),
+        array('allow', 
+          'actions'=>array('view', 'update'),
+          'roles'=>array('dbmanager', 'admin'),
+          ),
+        array('deny', 
+          'users'=>array('*'),
+          ),
+        );
+  }  
+  
+  /**
+   * As most of the game play is handled via JavaScript and API callbacks the controller
+   * renders only the initial needed HTML while making sure all needed assets CSS 
+   * and JavaScript are loaded 
+   */
+  public function actionIndex() {
+    MGHelper::setFrontendTheme();
+    
+    $game = GamesModule::loadGame("Mobile");
+    if ($game) {
+      $cs = Yii::app()->clientScript;
+      $cs->registerCoreScript('jquery');
+      $cs->registerCssFile(Yii::app()->baseUrl . '/css/jquery.fancybox-1.3.4.css');
+      $cs->registerCssFile(GamesModule::getAssetsUrl() . '/mobile/css/style.css');
+      $cs->registerScriptFile(Yii::app()->baseUrl . '/js/jquery.fancybox-1.3.4.pack.js', CClientScript::POS_END);
+      $cs->registerScriptFile(Yii::app()->baseUrl . '/js/jquery.tmpl.min.js', CClientScript::POS_END);
+      $cs->registerScriptFile(Yii::app()->baseUrl . '/js/mg.api.js', CClientScript::POS_END);
+      $cs->registerScriptFile(Yii::app()->baseUrl . '/js/mg.game.api.js', CClientScript::POS_END);
+      $cs->registerScriptFile(GamesModule::getAssetsUrl() . '/mobile/js/mg.game.mobile.js', CClientScript::POS_END);
+      $throttleInterval = (int)Yii::app()->fbvStorage->get("settings.throttle_interval", 1500);
+      $asset_url = Yii::app()->baseUrl;
+      $arcade_url = Yii::app()->getRequest()->getHostInfo() . Yii::app()->createUrl('/');
+      
+      $js = <<<EOD
+    MG_GAME_MOBILE.init({
+        gid : 'Mobile',
+        app_id : 'MG_API',
+        asset_url : '$asset_url',
+        api_url : '{$game->api_base_url}',
+        arcade_url : '$arcade_url',
+        game_base_url : '{$game->game_base_url}',
+        play_once_and_move_on : {$game->play_once_and_move_on},
+        play_once_and_move_on_url : '{$game->play_once_and_move_on_url}',
+        throttleInterval : $throttleInterval
+      });
+EOD;
+
+      // INSERT DEBUGGING HERE...
+      //error_log("MobileController: game var: " . var_export($game->more_info_url, true), 0);
+
+      Yii::app()->clientScript->registerScript(__CLASS__.'#game', $js, CClientScript::POS_READY);
+      
+      if ($game->play_once_and_move_on == 1) {
+        $this->layout = '//layouts/main_no_menu';
+      } else {
+        $this->layout = '//layouts/column1';
+      }
+      $this->render('index', array(
+        'game' => $game,
+      ));  
+    } else {
+      throw new CHttpException(403, Yii::t('app', 'The game is not active.'));
+    }
+  }
+  
+  /**
+   * show the game's settings
+   */
+  public function actionView() {
+    $model = $this->loadModel(array("unique_id" => "Mobile"), 'Mobile');  
+    $model->fbvLoad();
+    
+    $this->render('view', array(
+      'model' => $model,
+      'statistics' => GamesModule::getStatistics($model->id)
+    ));
+  }
+  
+  /**
+   * edit the game's settings
+   */
+  public function actionUpdate() {
+    $model = $this->loadModel(array("unique_id" => "Mobile"), 'Mobile');
+    $model->fbvLoad();
+    
+    $this->performAjaxValidation($model, 'mobile-form');
+    if (isset($_POST['Mobile'])) {
+      $model->setAttributes($_POST['Mobile']);
+      
+      $relatedData = array(
+        'imageSets' => $_POST['Mobile']['imageSets'] === '' ? null : $_POST['Mobile']['imageSets'],
+        'plugins' => $_POST['Mobile']['plugins'] === '' ? null : $_POST['Mobile']['plugins'],
+        );
+      
+      // save the games data in the database
+      if ($model->saveWithRelated($relatedData)) {
+        $model->fbvSave(); // but also save it in the settings file as each game uses FBVstorage
+        
+        MGHelper::log('update', 'Game ' . $model->name . ' updated');
+        Flash::add('success', $model->name . ' ' . Yii::t('app', "Updated"));
+        $this->redirect(array('view'));
+      }
+    }
+    
+    $this->render('update', array(
+      'model' => $model,
+      ));
+  }
+}
---
+++ www/protected/modules/games/views/mobile/index.php
@@ -0,0 +1,154 @@
+<div id="gamearea">
+  <div id="no_js">Unfortunately we can't show the game as it relies on JavaScript which appears to be disabled on your browser.</div>
+  <!-- Images from the database appear here --> 
+  <div id="stage">
+    <!-- The #holder div contains both the primary image for tagging
+         as well as the set of all images displayed at the end of the
+         game (added-in via javascript) -->
+    <div id="holder">
+      <div id="image_container" class="clearfix"></div>
+    </div>
+
+    <div id="scores"></div>
+    <!-- The ability to pass on a given image -->
+    <div id="passing">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+
+  </div> 
+</div>
+
+<!-- The bounding-box around the text input and the button -->
+<div id="input_area">
+  <form action="#">
+    <!-- user text field -->
+    <input type="text" name="words" id="words" placeholder="List what is in this image, separated by commas" />
+    <a href="#" id="button-play" class="ir"></a> 
+  </form> 
+</div>
+
+<div id="fieldholder" class="clearfix">
+</div>  
+
+<!-- New slide-out panel -->
+<div id="sidepanel">
+  <div id="tab" class="tab_closed"></div>
+  <div class="content">
+    <!-- In the mockup, looks like there's supposed to be a skinnier
+         DIV comprising the top section -->
+    <div class="skinny">
+      <br />
+      <br />
+      
+      <p>The Mobile Games interface/"Game" is part of the Metadata Games suite.</p>
+      
+      <p>Enter words that are relevant to the image. Separate words with a comma.</p>
+      <br />
+      <br />
+      <br />
+      <p>Metadata Games is an online game system for gathering useful data on photo, audio, and moving image artifacts.</p>
+      <div id="more_info"></div>
+      
+      <br />
+      <div id="licences"></div>
+    </div>
+
+    <div id="smallfooter">
+      &copy; <?php echo date('Y'); ?> <a href="http://www.tiltfactor.org/">tiltfactor</a>, all rights reserved
+      <div id="footerLogos">
+	<a href="http://www.dartmouth.edu" target="_blank">
+	  <img src="<?php echo MGHelper::bu("/"); ?>images/dartmouth_logo_20120116.jpg" />
+	</a>
+	<a href="http://www.neh.gov" target="_blank">
+	  <img src="<?php echo MGHelper::bu("/"); ?>images/NEH-Logo-Horizontal_252x62.jpg" />
+	</a>
+	<a href="http://www.acls.org/" target="_blank">
+	  <img src="<?php echo MGHelper::bu("/"); ?>images/acls-logo.gif" />
+	</a>
+      </div>
+    </div> <!-- smallfooter -->
+    
+  </div>
+</div>
+
+<script id="template-scores" type="text/x-jquery-tmpl">
+<!--
+  <h2>Welcome ${user_name}</h2>
+  <div class="total_score">You played <span>${user_num_played}</span> times and scored <span>${user_score}</span> Points</div>
+  <div class="current_score">This game's score <span>${current_score}</span> Points</div>
+  <div class="total_turns">Turn <span>${current_turn} </span>/<span> ${turns}</span></div>
+-->
+</script>
+<script id="template-licence" type="text/x-jquery-tmpl">
+  <h4>${name}</h4>
+  <p>${description}</p>
+</script> 
+<script id="template-turn" type="text/x-jquery-tmpl">
+  <div style="text-align:center" class="clearfix">
+    <img src="${url}" alt="game image" id="image_to_tag" />
+  </div>
+</script>
+<script id="template-final-summary" type="text/x-jquery-tmpl">
+  <table id="image_review">
+    <tr>
+      <td>
+        <div class="smallholder_left"> 
+          <a href="${url_full_size_1}" rel="zoom" title="${licence_info_1}">
+            <img class="scoreimages" src="${url_1}" alt="game image" />
+          </a>
+        </div>
+      </td>
+      <td>
+        <div class="smallholder_right"> 
+          <a href="${url_full_size_2}" rel="zoom" title="${licence_info_2}">
+            <img class="scoreimages" src="${url_2}" alt="game image" />
+          </a>
+        </div> 
+      </td>
+   </tr>
+   <tr>
+      <td>
+        <div class="smallholder_left"> 
+          <a href="${url_full_size_3}" rel="zoom" title="${licence_info_3}">
+            <img class="scoreimages" src="${url_3}" alt="game image" />
+          </a>
+        </div> 
+      </td>
+      <td>
+        <div class="smallholder_right"> 
+          <a href="${url_full_size_4}" rel="zoom" title="${licence_info_4}">
+            <img class="scoreimages" src="${url_4}" alt="game image" />
+          </a>
+        </div>
+      </td>
+   </tr>
+ </table>
+</script>
+<script id="template-final-summary-play-once" type="text/x-jquery-tmpl">
+  <div style="text-align:center" class="clearfix">
+    <a href="${url_full_size}" rel="zoom" title="${licence_info}"><img src="${url}" alt="game image" /></a>
+  </div>
+</script>
+<script id="template-more-info" type="text/x-jquery-tmpl">
+  <a href="${url}">Click here to learn more about ${name}</a>
+</script>
+<script id="template-final-info" type="text/x-jquery-tmpl">
+  <p class="final">You have earned ${current_score} point(s)! Thank you for playing.</p>
+  <a href="#" id="button-play-again" class="ir"><span>Play Again</span></a> 
+</script>
+<script id="template-final-tags-new" type="text/x-jquery-tmpl">
+  <p class="tag-info"><!-- No tags displayed in Mobile --></p>
+</script>
+<script id="template-final-tags-matched" type="text/x-jquery-tmpl">
+  <p class="tag-info"><!-- No tags displayed in Mobile --></p>
+</script>
+<script id="template-final-info-play-once" type="text/x-jquery-tmpl">
+  You'll be redirected in <span id="remainingTime">${remainingTime}</span> seconds. <a href="${play_once_and_move_on_url}">Click here to proceed right away.</a></p>
+</script>
+<script id="template-info-modal-critical-error" type="text/x-jquery-tmpl">
+  ${error} <p>Return to the <a href="${arcade_url}">arcade</a>.</p>
+</script>
