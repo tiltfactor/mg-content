6daaadf5ea76c1c30cfbf6c647136bf01befef0d
Delchina Angelova <delchina@sirma.bg>
2013-08-05 13:42:05 +0300

    implement the missing :active graphic

--- www/protected/modules/games/assets/pyramid/js/mg.game.pyramid_splash.js
+++ www/protected/modules/games/assets/pyramid/js/mg.game.pyramid_splash.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
     onResize();
     $("#footer").hide();
+    $(".hover_btn").hover(
+        function () {
+            this.src = this.src.replace("_off","_on");
+        }, function() {
+            this.src = this.src.replace("_on","_off");
+        }
+    )
 });
 $(window).resize(function() {
     onResize ();
@@ -8,5 +15,4 @@ $(window).resize(function() {
 function onResize () {
     $("#splash_home .middle_height").css("max-height", $(window).height() / 3);
     $("#splash_logo").centerVertival();
-//    $("#splash_logo").css({"max-width": $(window).width(), "max-height": $(window).height() / 3 });
 }
