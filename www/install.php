f6d3b261710acd772fcdebb534a39211815a4467
Delchina Angelova <delchina@sirma.bg>
2013-08-12 11:38:33 +0300

    Add notice for missing line in php.ini

--- www/install.php
+++ www/install.php
@@ -1,5 +1,12 @@
 <?php
 // change the following paths if necessary
+try {
+    $now = new DateTime('now');
+} catch (Exception $e) {
+    echo 'Since PHP 5.1.0 (when the date/time functions were rewritten). Update your php.ini file and restart.';
+    die();
+}
+
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/installer.php';
 
