0c7524243f2047913b8eb8015f84e7af06c98812
Nikolay Kondikov <nikolay.kondikov@sirma.bg>
2013-09-16 17:53:54 +0300

    Bug fixes

--- www/protected/controllers/ImageController.php
+++ www/protected/controllers/ImageController.php
@@ -6,17 +6,20 @@
 class ImageController extends Controller
 {
 
-    public function actionScaled($token,$name,$width,$height){
+    public function actionScale($token, $name, $width, $height)
+    {
         $institutionToken = Yii::app()->fbvStorage->get("token");
 
-        $verifyToken = md5($institutionToken."_".$width."_".$height."_name");
-        if($verifyToken == $token){
-            $path= realpath(Yii::app()->getBasePath() . Yii::app()->fbvStorage->get("settings.app_upload_path"));
+        $name = urldecode($name);
+
+        $verifyToken = md5($institutionToken . "_" . $width . "_" . $height . "_" . $name);
+        if ($verifyToken == $token) {
+            $path = realpath(Yii::app()->getBasePath() . Yii::app()->fbvStorage->get("settings.app_upload_path"));
             $imgName = MGHelper::createScaledMedia($name, "", "scaled", $width, $height, 80, 10);
 
             $file = $path . "/scaled/" . $imgName;
             $request = Yii::app()->getRequest();
-            $request->sendFile($imgName,file_get_contents($file));
+            $request->sendFile($imgName, file_get_contents($file));
         }
     }
 }
