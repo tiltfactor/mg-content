26a4c841642407a4c9e2ef0ca89dea9e69d656a5
sukie punjasthitkul <sukie@tiltfactor.org>
2013-09-22 14:02:55 -0400

    Fix yiic.php to access migrate, other commands
    Related to jira issue MDG-99: Update DB, yiic not working as expected.
    
    Fix yiic.php so that you can run 'yiic migrate' or other
    yiic commands from the commandline.

--- www/protected/yiic.php
+++ www/protected/yiic.php
@@ -4,6 +4,10 @@
 $yii=dirname(__FILE__).'/../framework/yii.php';
 $config=dirname(__FILE__).'/config/main.php';
 
+// fix for fcgi
+// defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
+
+
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
@@ -11,4 +15,19 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 
-$app = Yii::createConsoleApplication($config)->run();
+// $app = Yii::createConsoleApplication($config)->run();
+
+
+if(isset($config))
+{
+        $app=Yii::createConsoleApplication($config);
+        $app->commandRunner->addCommands(YII_PATH.'/cli/commands');
+}
+else
+        $app=Yii::createConsoleApplication(array('basePath'=>dirname(__FILE__).'/cli'));
+
+$env=@getenv('YII_CONSOLE_COMMANDS');
+if(!empty($env))
+        $app->commandRunner->addCommands($env);
+
+$app->run();
